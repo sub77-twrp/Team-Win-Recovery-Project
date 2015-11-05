57c6db75d4c4d674e480c73a9a62c6c764ad0cd2
maxwen <max.weninger@windriver.com>
2015-11-05 13:22:55 +0100

    minuitwrp: pixelflinger no longer has VectorImpl
    after latest google CVE. So add libutils
    
    Change-Id: I86e0c8a23018fae1c4097a9993c8fa7dbbb7c64e

--- minuitwrp/Android.mk
+++ minuitwrp/Android.mk
@@ -157,7 +157,7 @@ ifneq ($(TW_THEME_LANDSCAPE),)
   LOCAL_CFLAGS += -DTW_HAS_LANDSCAPE
 endif
 
-LOCAL_SHARED_LIBRARIES += libz libc libcutils libjpeg libpng
+LOCAL_SHARED_LIBRARIES += libz libc libcutils libjpeg libpng libutils
 LOCAL_STATIC_LIBRARIES += libpixelflinger_static
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE := libminuitwrp
